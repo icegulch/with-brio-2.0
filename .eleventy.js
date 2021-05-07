f0f1f5a8ab2d6f1ba69bf1f80d6fb83d9dfed3c7
Steven Bailey <stevenbailey@Stevens-MacBook-Pro.local>
2021-05-06 20:10:35 -0600

    minify again?

--- .eleventy.js
+++ .eleventy.js
@@ -58,6 +58,11 @@ module.exports = function (eleventyConfig) {
   return {
     addPassthroughCopy: true,
     templateFormats: ["md", "njk", "html"],
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
